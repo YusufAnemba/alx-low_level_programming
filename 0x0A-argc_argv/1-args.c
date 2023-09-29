cb9c1f22716a7e7c270fe49bb5a7d83fa9503ac4
Yusuf Anemba <yusufanemba001@gmail.com>
2023-09-29 16:06:57 -0700

    new arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main-  program prints a number, followed by a new line
+ *@argc: count number of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
