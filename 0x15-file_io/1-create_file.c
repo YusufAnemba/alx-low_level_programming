0e8ff3d58cffdbf463b00662dfa05a18ef56fb05
Yusuf Anemba <yusufanemba001@gmail.com>
2023-11-08 07:13:08 -0800

    3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	rwr = write(file_f, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
